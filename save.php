49fe62097d97c5e666e8ccd796610a28424147c1
ssss89 <115527387+ssss89@users.noreply.github.com>
2022-10-18 18:24:37 +0530

    signup page

--- save.php
+++ save.php
@@ -1,57 +1,55 @@
 <?php 
 include "db.php";
-if(isset($_POST['sdfg']))
+if(isset($_FILES['avtar'])){
+
+   $file_name = $_FILES['avtar']['name'];
+   $file_size =  $_FILES['avtar']['size'];
+   $file_tmpname =  $_FILES['avtar']['tmp_name'];
+   $ext = strtolower(end(explode('.', $file_name)));
+   $exti = array("jpeg","jpg","png");
+
+   if(in_array($ext,$exti)== true & $file_size <=2100000){
+
+      move_uploaded_file($file_name,"profile/". $file_name );
+   }else{
+      header("location:signup.php");
+   }
+}
+
+
+if(isset($_POST['profile']))
 {
+   $fname = mysqli_real_escape_string($conn,$_POST['name']);
+   $funame = mysqli_real_escape_string($conn,$_POST['username']);
+   $femail = mysqli_real_escape_string($conn,$_POST['email']);
+
+  $sqlc = "SELECT * FROM register WHERE username = '{$funame}' ";
+
+  $resc = mysqli_query($conn,$sqlc);
+
+  if(mysqli_num_rows($resc)>0){
+
+   header("location:signup.php");
+
+  }else{
+
+
+//$sql = "INSERT INTO `register`('firstname','username','email','filename') VALUES('{$fname}','{$funame}','{$femail}','{$file_name}')";
+$sql = "INSERT INTO `register` (`firstname`, `username`, `email`, `filename`) VALUES ( '{$fname}', '{$funame}', '{$femail}', '{$file_name}')";
+$res = mysqli_query($conn,$sql);
+
+if($res == true){
+   header("location:profile.php");
+}else{
+   header("location:signup.php");
+}
+
+
+  }
    
-    $fname = $_POST['name'];
-    $femail = $_POST['email'];
-    $funame = $_POST['username'];
-    //echo  $fname ."<br>". $femail . "<br>". $funame;
 
+   
 
-
-    /**if(isset($_FILES['filetoupde']))
-    {
-        $filenames = $_FILES['filetouplode']['name'];
-        $filesize = $_FILES['filetouplode']['size'];
-        $filetmpnames = $_FILES['filetouplode']['tmp_name'];
-        $filetype = $_FILES['filetouplode']['type'];
-        $ext = strtolower(end(explode(".",$filenames)));
-        $exti = array("jpeg","jpg", "png");
-        
-        move_uploaded_file($filenames,"a/uplode/" .$filenames);
-
-
-
-    }**/
-
-    if(isset($_FILES['image'])){
-        $errors= array();
-        $file_name = $_FILES['image']['name'];
-        $file_size =$_FILES['image']['size'];
-        $file_tmp =$_FILES['image']['tmp_name'];
-        $file_type=$_FILES['image']['type'];
-        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-        
-        $extensions= array("jpeg","jpg","png");
-        print_r($file_name). "</h2>" ;
-        DIE();
-        }
-        
-        /**if(in_array($file_ext,$extensions)=== false){
-           $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-        }
-        
-        if($file_size > 2097152){
-           $errors[]='File size must be excately 2 MB';
-        }
-        **/
-        if(empty($errors)==true){
-           move_uploaded_file($file_tmp,"a/uplode/".$file_name);
-           echo "Success";
-        }else{
-             print_r($file_name);
-     }
 }
 
 
